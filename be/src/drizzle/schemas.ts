f50b5d9ddd3ee07444f160e3362c5c5fe1238ab9
Alan Kinkela Ramirez <51862792+Akinkelar@users.noreply.github.com>
2023-08-25 08:33:14 +0200

    reinit

--- be/src/drizzle/schemas.ts
+++
@@ -1,7 +0,0 @@
-import { text, sqliteTable, integer } from 'drizzle-orm/sqlite-core';
-
-export const users = sqliteTable('user', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  email: text('email'),
-  password: text('password'),
-});
